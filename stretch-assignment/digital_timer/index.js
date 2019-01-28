9b9d0dc72cc6d0fa84a890a2ed563472023727f1
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-01-28 18:04:57 -0500

    added button to start timer

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,18 +1,23 @@
 window.onload = () => {
-  let startTime = Date.now();
-  const digits = Array.from(document.querySelectorAll(".digit:not(#colon)"));
-  const interval = setInterval(() => {
-    let currentTime = Date.now() - startTime;
-    if(currentTime > 10000) {
-      digits.forEach(d => d.style.color = "red");
-      document.getElementById("colon").style.color = "red";
-      currentTime = 10000;
-      clearInterval(interval);
-    }
+  const btn = document.createElement("button");
+  btn.textContent = "start";
+  document.querySelector("body").appendChild(btn);
+  btn.onclick = () => {
+    let startTime = Date.now();
+    const digits = Array.from(document.querySelectorAll(".digit:not(#colon)"));
+    const interval = setInterval(() => {
+      let currentTime = Date.now() - startTime;
+      if(currentTime > 10000) {
+        digits.forEach(d => d.style.color = "red");
+        document.getElementById("colon").style.color = "red";
+        currentTime = 10000;
+        clearInterval(interval);
+      }
 
-    let timeChars = Math.floor(currentTime / 10).toString().padStart(4, '0');
-    for(let i = 0; i < timeChars.length; i++) {
-      digits[i].textContent = timeChars[i];
-    }
-  }, 10);
+      let timeChars = Math.floor(currentTime / 10).toString().padStart(4, '0');
+      for(let i = 0; i < timeChars.length; i++) {
+        digits[i].textContent = timeChars[i];
+      }
+    }, 10);
+  };
 };
